4c4fcbd5c876b2583304792ef4c227022ee695b9
Agonn <Agon.r@msn.com>
2017-07-26 22:11:09 +0200

    New addition to galery and index

--- galeria.php
+++ galeria.php
@@ -1,89 +1,100 @@
-<?php require_once( 'couch/cms.php' ); ?>
-<cms:template title="Admin panel">
-<cms:repeatable name='galeria' label="Shto foto" >
-	<cms:editable type='image' name='images' show_preview='1' preview_width='150' label='Foto' />
-	<cms:editable type='textarea' name='description' label='Pershkrimi' />
-</cms:repeatable>
+<?php require_once( 'couch/cms.php');
+?>
+<cms:template title="Galeria kryesore">
+  <cms:repeatable name='galeria' label="Shto foto">
+    <cms:editable type='image' name='images' show_preview='1' preview_width='150' label='Foto' />
+    <cms:editable type='textarea' name='description' label='Pershkrimi' /></cms:repeatable>
 </cms:template>
-
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<title>Astrit Istogu | Galeria</title>
-
-	<!-- CSS -->
+  <meta charset="utf-8">
+  <title>Astrit Istogu | Galeria</title>
+  <!-- CSS -->
   <link rel="stylesheet" type="text/css" href="\fancybox\dist\jquery.fancybox.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
   <style>
+    #photos {
+      /* Prevent vertical gaps */
+      line-height: 0;
+      -webkit-column-count: 3;
+      -webkit-column-gap: 0px;
+      -moz-column-count: 3;
+      -moz-column-gap: 0px;
+      column-count: 3;
+      column-gap: 0px;
+    }
 
-#photos {
-   /* Prevent vertical gaps */
-   line-height: 0;
-   
-   -webkit-column-count: 5;
-   -webkit-column-gap:   0px;
-   -moz-column-count:    5;
-   -moz-column-gap:      0px;
-   column-count:         4;
-   column-gap:           0px;
-}
+    #photos img {
+      /* Just in case there are inline attributes */
+      width: 100% !important;
+      height: auto !important;
+    }
 
-#photos img {
-  /* Just in case there are inline attributes */
-  width: 100% !important;
-  height: auto !important;
-}
+    @media (max-width: 1200px) {
+      #photos {
+        -moz-column-count: 4;
+        -webkit-column-count: 4;
+        column-count: 4;
+      }
+    }
 
-@media (max-width: 1200px) {
-  #photos {
-  -moz-column-count:    4;
-  -webkit-column-count: 4;
-  column-count:         4;
-  }
-}
-@media (max-width: 1000px) {
-  #photos {
-  -moz-column-count:    3;
-  -webkit-column-count: 3;
-  column-count:         3;
-  }
-}
-@media (max-width: 800px) {
-  #photos {
-  -moz-column-count:    2;
-  -webkit-column-count: 2;
-  column-count:         2;
-  }
-}
-@media (max-width: 400px) {
-  #photos {
-  -moz-column-count:    1;
-  -webkit-column-count: 1;
-  column-count:         1;
-  }
-}
+    @media (max-width: 1000px) {
+      #photos {
+        -moz-column-count: 3;
+        -webkit-column-count: 3;
+        column-count: 3;
+      }
+    }
 
-body {
-  margin: 0;
-  padding: 0;
-}
+    @media (max-width: 800px) {
+      #photos {
+        -moz-column-count: 2;
+        -webkit-column-count: 2;
+        column-count: 2;
+      }
+    }
 
+    @media (max-width: 400px) {
+      #photos {
+        -moz-column-count: 1;
+        -webkit-column-count: 1;
+        column-count: 1;
+      }
+    }
 
-</style>
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    .fixed-action-btn1 {
+      position: fixed;
+      left: 23px;
+      bottom: 23px;
+      padding-top: 15px;
+      margin-bottom: 0;
+      z-index: 50000;
+    }
+  </style>
 </head>
-<body><section id="photos">
-  <cms:show_repeatable 'galeria' >
-    
-<div class="gallery">
-	<a href="<cms:show images/>" data-fancybox="group" data-fancybox data-caption="<cms:show description/>">
-	<img src="<cms:show images/>" alt="<cms:show description/>" />
-</a>
-</cms:show_repeatable>
-</section>
 
-	<!-- JS -->
-	<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script src="\fancybox\dist\jquery.fancybox.min.js"></script>
+<body>
+  <section id="photos">
+    <cms:show_repeatable 'galeria'>
+      <div class="gallery"><a href="<cms:show images/>" data-fancybox="group" data-fancybox data-caption="<cms:show description/>"><img src="<cms:show images/>" alt="<cms:show description/>" /></a></cms:show_repeatable>
+    <div
+      class="fixed-action-btn1"><a href="index.php" class="btn-floating btn-large red"><i class="large material-icons">arrow_back</i></a></section>
+  <!-- JS -->
+  <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="\fancybox\dist\jquery.fancybox.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
 </body>
+
 </html>
-<?php COUCH::invoke(); ?>
+<?php COUCH::invoke();
+?>
--- index.php
+++ index.php
@@ -1,177 +1,165 @@
-<?php require_once( 'couch/cms.php' ); ?>
-<cms:template title="Faqja kryesore">
-<cms:editable name='prop_uploads' label='Fotot ne background' desc='Fotot ne prapavije - Faqja kryesore' type='group' />
-
-<cms:editable name='image_1' label='Foto per faqen e pare'  show_preview='1' preview_width='250'
-    group='prop_uploads'
-    type='image' />
-
-    <cms:editable name='image_2' label='Foto per faqen e dyte'  show_preview='1' preview_width='250'
-''    group='prop_uploads'
-    type='image' />
-
-    
-    <cms:editable name='image_3' label='Foto per faqen e trete'  show_preview='1' preview_width='250'
-    group='prop_uploads'
-    type='image' />
-
-    <cms:editable name='image_1_text' group='prop_uploads' label='Titulli per slide 1' desc='Shkruani titullin per slide 1'
-    maxlength='20'
+<?php require_once( 'couch/cms.php');
+?>
+<cms:template title="Faqja kryesore" >
+  <cms:editable name='prop_uploads' label='Fotot ne background' desc='Fotot ne prapavije - Faqja kryesore' type='group' />
+  <cms:editable name='image_1' label='Foto per faqen e pare' show_preview='1' preview_width='250' group='prop_uploads' type='image'
+  />
+  <cms:editable name='image_2' label='Foto per faqen e dyte' show_preview='1' preview_width='250' group='prop_uploads' type='image'
+  />
+  <cms:editable name='image_3' label='Foto per faqen e trete' show_preview='1' preview_width='250' group='prop_uploads' type='image'
+  />
+  <cms:editable name='image_1_text' group='prop_uploads' label='Titulli per slide 1' desc='Shkruani titullin per slide 1' maxlength='20'
     type='text' />
-
-
-       <cms:editable name='image_2_text' group='prop_uploads' label='Titulli per slide 2' desc='Shkruani titullin per slide 2'
-    maxlength='20'
+  <cms:editable name='image_2_text' group='prop_uploads' label='Titulli per slide 2' desc='Shkruani titullin per slide 2' maxlength='20'
     type='text' />
-
-       <cms:editable name='image_3_text' group='prop_uploads' label='Titulli per slide 3' desc='Shkruani titullin per slide 3'
-    maxlength='20'
-    type='text' />
-
-</cms:template>
-
-
+  <cms:editable name='image_3_text' group='prop_uploads' label='Titulli per slide 3' desc='Shkruani titullin per slide 3' maxlength='20'
+    type='text' /></cms:template>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
   <title>Astrit Istogu Photography</title>
-
-  <!-- CSS  -->
+  <!-- CSS -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
   <style>
-.slider-adjustment{
-  top: 56px;  
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
-  background-color:cyan ;
-}
-.fixed-action-btn.toolbar ul li a:hover {
-  background-color:purple;
-}
+    .slider-adjustment {
+      top: 56px;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      overflow: auto;
+      background-color: cyan;
+    }
 
-    </style>
+    .fixed-action-btn.toolbar ul li a:hover {
+      background-color: purple;
+    }
+  </style>
 </head>
+
 <body>
   <!-- nav !-->
-<ul id="slide-out" style="z-index: 10001;"class="side-nav">
-    <li><div class="user-view">
-      <div class="background">
-        <img src="http://i.imgur.com/WeXE1wq.png">
+  <ul id="slide-out" style="z-index: 10001;" class="side-nav">
+    <li>
+      <div class="user-view">
+        <div class="background"><img src="http://i.imgur.com/WeXE1wq.png"></div><a href="#!user"><img class="circle" src="https://instagram.fprx1-1.fna.fbcdn.net/t51.2885-19/s150x150/18646100_295846277535127_6500728101108973568_a.jpg"></a>
+        <a href="#"><span class="white-text name">Astrit Istogu</span></a>
       </div>
-      <a href="#!user"><img class="circle" src="https://instagram.fprx1-1.fna.fbcdn.net/t51.2885-19/s150x150/18646100_295846277535127_6500728101108973568_a.jpg"></a>
-      <a href="#"><span class="white-text name">Astrit Istogu</span></a>
-    </div></li>
+    </li>
     <li><a class="subheader">Kontakti</a></li>
     <li><a href="tel:+37744503303"><i class="material-icons">phone</i>+377 44 503 303</a></li>
     <li><a href="mailto:astritiistogu@gmail.com"><i class="material-icons">email</i>astritiistogu@gmail.com</a></li>
-    <li><div class="divider"></div></li>
+    <li>
+      <div class="divider"></div>
+    </li>
     <li><a class="subheader">Social Media</a></li>
     <li><a href="https://www.instagram.com/astritistoguphotography/" target='_blank'><img src="images/insta.png">Astritistoguphotography</a></li>
     <li><a href="https://www.facebook.com/astrit.istogu/photos_all" target='_blank'><img src="images/fb.png">Astritistoguphotography</a></li>
-    <!--<li><a href="#!"><img src="images/snapchat.png">AstritiSnapchat</a></li> !-->
-      <li><div class="divider"></div></li>
-    <li><a class="subheader">Shpërndaje</a></li>
+    <!--<li><a href="#!"><img src="images/snapchat.png">AstritiSnapchat</a></li>!-->
     <li>
-      
- <a id="viber_share"><img src="images/viber.png">Viber</a>
-<script>
-    var buttonID = "viber_share";
-    var text = "Shiko këtë fotograf: ";
-    document.getElementById(buttonID)
-        .setAttribute('href', "https://3p3x.adj.st/?adjust_t=u783g1_kw9yml&adjust_fallback=https%3A%2F%2Fwww.viber.com%2F%3Futm_source%3DPartner%26utm_medium%3DSharebutton%26utm_campaign%3DDefualt&adjust_campaign=Sharebutton&adjust_deeplink=" + encodeURIComponent("viber://forward?text=" + encodeURIComponent(text + " " + window.location.href)));
-</script></li>
-<li><a href="whatsapp://send?text=http://astritistogu.com" data-action="share/whatsapp/share"><img src="images/whatsapp.png">Whatsapp</a>
-</li>
-    
-  </ul>
-  <a href="#" data-activates="slide-out" style="position: absolute;
-    z-index: 10000" class="button-collapse"><i class="medium material-icons">menu</i></a>
+      <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Shpërndaje</a></li>
+    <li><a id="viber_share"><img src="images/viber.png">Viber</a>
+      <script>
+        var buttonID = "viber_share";
+        var text = "Shiko këtë fotograf: ";
+        document.getElementById(buttonID).setAttribute('href',
+          "https://3p3x.adj.st/?adjust_t=u783g1_kw9yml&adjust_fallback=https%3A%2F%2Fwww.viber.com%2F%3Futm_source%3DPartner%26utm_medium%3DSharebutton%26utm_campaign%3DDefualt&adjust_campaign=Sharebutton&adjust_deeplink=" +
+          encodeURIComponent("viber://forward?text=" + encodeURIComponent(text + " " + window.location.href)));
+      </script>
+    </li>
+    <li><a href="whatsapp://send?text=http://astritistogu.com" data-action="share/whatsapp/share"><img src="images/whatsapp.png">Whatsapp</a></li>
+  </ul><a href="#" data-activates="slide-out" style="position: absolute;
+ z-index: 10000" class="button-collapse"><i class="medium material-icons">menu</i></a>
   <!-- /Nav !-->
   <!-- Content !-->
-<div class="slider-adjustment">
-
-<div class="slider fullscreen">
-  <ul class="slides">
-    <li>
-      <img src="<cms:show image_1/>"> 
-      <div class="caption center-align">
-        <h3><cms:show image_1_text/></h3>
-        <h5 class="light grey-text text-lighten-3"></h5>
-      </div>
-    </li>
-    <li>
-      <img src="<cms:show image_2/>"> 
-      <div class="caption left-align">
-        <h3><cms:show image_2_text/></h3>
-        <a href="#" data-activates="slide-out" class="btn button-collapse waves-light purple darken-4">Kontakti</a>
-        
-      </div>
-    </li>
-    <li>
-      <img src="<cms:show image_3/>"> 
-      <div class="caption right-align">
-        <h3><cms:show image_3_text/></h3>
-        <h5 class="light grey-text text-lighten-3">Instagram</h5>
-      </div>
-    </li>
-  </ul>
-</div>
-
-  <!-- /Content !-->
- 
-   <div class="fixed-action-btn toolbar ">
-    <a class="btn-floating btn-large red purple darken-4">
-      <i class="large material-icons">picture_in_picture</i>
-    </a>
-    <ul>
-    <!--<li class="waves-effect waves-light">  <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Dergo email</a></li>!-->
-      <li class="waves-effect waves-light"><a href="galeria.php"><i class="material-icons"></i>Galeria</a></li>
-      <li class="waves-effect waves-light"><a href="https://www.instagram.com/astritistoguphotography/" target='_BLANK'><i class="material-icons"></i>Instagram</a></li>
-    </ul>
-  </div>
-
-
-  <!-- Modal Structure 
-  <div id="modal1" class="modal bottom-sheet">
-    <div class="modal-content">
-      <h4>Modal Header</h4>
-      <p>A bunch of text</p>
+  <div class="slider-adjustment">
+    <div class="slider fullscreen">
+      <ul class="slides">
+        <li><img src="<cms:show image_1/>">
+          <div class="caption center-align">
+            <h3>
+              <cms:show image_1_text/>
+            </h3>
+            <h5 class="light grey-text text-lighten-3"></h5>
+          </div>
+        </li>
+        <li><img src="<cms:show image_2/>">
+          <div class="caption left-align">
+            <h3>
+              <cms:show image_2_text/>
+            </h3><a href="#" data-activates="slide-out" class="btn button-collapse waves-light purple darken-4">Kontakti</a></div>
+        </li>
+        <li><img src="<cms:show image_3/>">
+          <div class="caption right-align">
+            <h3>
+              <cms:show image_3_text/>
+            </h3>
+            <h5 class="light grey-text text-lighten-3">Instagram</h5>
+          </div>
+        </li>
+      </ul>
     </div>
-    <div class="modal-footer">
-      <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+    <!-- /Content !-->
+    <div class="fixed-action-btn toolbar "><a class="btn-floating btn-large red purple darken-4"><i class="large material-icons">picture_in_picture</i></a>
+      <ul>
+        <!--<li class="waves-effect waves-light"><a class="waves-effect waves-light btn modal-trigger" href="#modal1">Dergo email</a></li> !-->
+        <li class="waves-effect waves-light"><a href="https://www.instagram.com/astritistoguphotography/" target='_BLANK'><i class="material-icons"></i>Instagram</a></li>
+        <li class="waves-effect waves-light"><a href="galeria.php"><i class="material-icons"></i>Galeria</a></li>
+      </ul>
     </div>
-  </div>-->
+    <!-- Modal 
+     <div id="modal1" class="modal bottom-sheet"><div class="modal-content"> <form method="post" id="myForm">
+          <div class="form-group">
+            <label for="name">Your name:</label>
+            <input type="text" name="name" id="name" class="form-control" placeholder="Name" value="" required/>
+          </div>
+          <div class="form-group">
+            <label for="email">Your email:</label>
+            <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="" required/>
+          </div>
+		    <div class="form-group">
+            <label for="phone">Your phone:</label>
+            <input type="number" name="phone" id="phone" class="form-control" placeholder="Phone" value="" required/>
+          </div>
+          <div class="form-group">
+            <label for="comment">Your comment:</label>
+            <textarea class="form-control" id="comment" name="comment" required></textarea>
+          </div>
+          <input type="submit"  onclick="myFunction()" name="submit" class="btn btn-success btn-lg" value="Submit">
+        </form><div class="modal-footer"></div></div>-->
+    <!-- Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+    <script>
+      $(document).ready(function () {
+          $('.slider').slider({
+            transition: 300,
+            interval: 10000,
+            indicators: true
+          });
+        }
 
+      );
+      $('.button-collapse').sideNav({
+          menuWidth: 300, // Default is 300
+          edge: 'left', // Choose the horizontal origin
+          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+          draggable: true
+        }
 
+      );
+      $('.modal').modal();
+    </script>
+</body>
 
-  <!--  Scripts-->
-  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
-  <script>
-      
-  
-    $(document).ready(function(){
-      $('.slider').slider({transition:300,interval:10000,indicators:true});
-    });
- 
- $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true
- });
- $('.modal').modal();
-      
-</script>
-
-  </body>
 </html>
-<?php COUCH::invoke(); ?>
+<?php COUCH::invoke();
+?>
